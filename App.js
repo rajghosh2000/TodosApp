f001339471686cc9ee5449fb2b0ba8ea21e80d21
Rajdeep Ghosh <rikta_raj@rediffmail.com>
2020-08-18 22:15:51 +0530

    ...

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 40}}>
+      <View>
+        <TextInput
+          placeholder="Enter your todo here"
+          style={{
+            borderColor : 'black',
+            borderWidth: 2,
+            padding : 10
+          }}
+        />
+        <Button
+          title="ADD"
+        />
+      </View>
+      <View>
+
+      </View>      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
