0401b02cdf3140c9bfc95cae5fad74ea7df2d536
Rajdeep Ghosh <rikta_raj@rediffmail.com>
2020-08-19 20:12:11 +0530

    ..

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from "react-native";
 
 export default function App() {
-
-
-  const [enteredGoal, setEnteredGoal] = useState('');
+  const [enteredGoal, setEnteredGoal] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
   const goalInputHandler = (enteredText) => {
@@ -13,66 +11,84 @@ export default function App() {
   };
 
   const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...courseGoals, enteredGoal]);
+    setCourseGoals(currentGoals => [...currentGoals, {
+      gid : Math.random().toString(),
+      value : enteredGoal
+    }]);
   };
 
   return (
     <View style={styles.screen}>
-      <View style={styles.main}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            placeholder="Enter your todo here"
-            style={styles.input}
-            onChangeText={goalInputHandler}
-            value={enteredGoal}
-          />
-          <Button
-            title="ADD"
-            onPress={addGoalHandler}
-          />
-        </View>
-        <View>
-          {courseGoals.map((goal) =>
-            <View  
-              key={goal} 
-              style={styles.listItram}
-            >
-              <Text>
-                {goal}
-              </Text>
-            </View>
-          )}
-        </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter your todo here"
+          placeholderTextColor="black"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button 
+          title="ADD" 
+          onPress={addGoalHandler} 
+          style={styles.addButton}
+        />
       </View>
+      <FlatList
+        keyExtractor={(item,index) => item.gid}
+        data={courseGoals}
+        renderItem={itemData => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}      
+      />     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 40,
-  },
-  main: {
-    backgroundColor: '#0A3D62',
+    padding: 50,
+    backgroundColor: "#0A3D62",
     flex: 1,
   },
+
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignContent: 'center',
-    tintColor: 'green'
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+    tintColor: "green",
+    
   },
   input: {
-    width: '80%',
-    borderColor: 'black',
-    borderWidth: 2,
-    padding: 10
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 25,
+    overflow: 'hidden',
   },
-  listItram: {
+  addButton:{
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 25,
+
+  },
+  listItem: {
     padding: 10,
     marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1
-  }
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 25,
+    overflow: 'hidden',
+  },
 });
